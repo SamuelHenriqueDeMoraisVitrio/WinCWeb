e57c91564b2316f99dc5da19f00dfccc1cd11836
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2024-11-20 03:59:08 -0300

    mody

--- src/imports.h
+++ src/imports.h
@@ -1,17 +1,24 @@
 #ifndef PRIVATE_CWEB_HTTP_IMPORTS_H
 #define PRIVATE_CWEB_HTTP_IMPORTS_H
 
-#include <sys/wait.h>
+#include <sys/wait.h>//NOT
 #include <stdio.h>
-#include <sys/stat.h>
+#include <sys/stat.h>//NOT
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <signal.h>
-#include <sys/resource.h>
+#include <sys/socket.h>//NOT
+#include <signal.h>//NOT~
+#include <sys/resource.h>//NOT
 
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <arpa/inet.h>//NOT
+#include <unistd.h>//NOT
+
+//Windows
+/*
+#include <winsock2.h>   // Substitui sys/socket.h e arpa/inet.h
+#include <windows.h>    // Substitui unistd.h, sys/resource.h e sys/wait.h
+#include <io.h>         // Substitui algumas funções de sys/stat.h
+*/
 
 #endif //PRIVATE_CWEB_HTTP_IMPORTS_H
